e1fa46bd09783a73cc69025e0f7e0b6f0d8862ab
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2024-05-06 10:03:09 +0530

    hi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,38 @@
-import React from 'react'
-
+import { useState, useEffect } from 'react';
+import React from 'react';
+import Lenis from "@studio-freight/lenis";
+import Preloader from '../components/Preloader'
+import Menu from '@/components/Menu';
+import BackgroundBeams from '@/components/BackgroundBeams'; // Import the BackgroundBeams component
+import { DotBackgroundDemo } from '@/components/DotBackgroundDemo';
+import Features from '@/components/Features';
+import { CardStackDemo } from '@/components/CardStackDemo';
+import MarqueeDemo from '@/components/MarqueeDemo';
+import DownloadBadge from '@/components/DownloadBadge';
 function index() {
+  useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger);
+		const lenis = new Lenis()
+		function raf(time) {
+		  lenis.raf(time)
+		  requestAnimationFrame(raf)
+		}
+		requestAnimationFrame(raf)
+		
+	}, []);
+
   return (
-    <div>
-      start building from here
-    </div>
+    <div>  
+      <Preloader />
+      <Menu />
+      <BackgroundBeams />
+	  <DotBackgroundDemo />
+	  <Features />
+	  <MarqueeDemo />
+	  <CardStackDemo />
+	  <DownloadBadge />
+ </div>    
+
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+ 
 module.exports = {
+  
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +21,57 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+      },
     },
   },
-  plugins: [],
+
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
